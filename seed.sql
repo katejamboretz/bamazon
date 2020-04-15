0bad3b8186b9434b5009abea04168961770aede6
Kate Jamboretz <kejamboretz@dons.usfca.edu>
2020-04-14 18:11:05 -0700

    Add ten rows to products table in seed.sql

---
+++ seed.sql
@@ -0,0 +1,20 @@
+USE bamazon_DB;
+
+INSERT INTO
+    products (
+        product_name,
+        department_name,
+        price,
+        stock_quantity
+    )
+VALUES
+    ("Toothpaste", "Toiletries", 7, 200),
+    ("Bananas", "Groceries", 5, 50),
+    ("Toilet Paper", "Toiletries", 20, 100),
+    ("DVD Player", "Electronics", 17, 25),
+    ("Bowls", "Kitchen", 25, 46),
+    ("Earbuds", "Electronics", 150, 50),
+    ("Popcorn", "Groceries", 3, 355),
+    ("Blankets", "Bedding", 23, 100),
+    ("Towels", "Bathroom", 30, 77),
+    ("Dish Soap", "Kitchen", 4, 230);
