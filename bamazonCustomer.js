61edc23b7ba1301f2a45e00dd626676aed2c3aa1
Kate Jamboretz <kejamboretz@dons.usfca.edu>
2020-04-14 21:33:44 -0700

    update howMuch() function to request the amount and determine the cost

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -43,25 +43,61 @@ function what() {
       message: "\n\nWhat is the ID of the item you would you like to buy?",
     })
     .then(function (answer) {
-      idNumber = answer.what;
+      var idNumber = answer.what;
       var query = connection.query(
         "SELECT * FROM products WHERE ?",
         { id: idNumber },
         function (err, res) {
           if (err) throw err;
-          console.table(res);
+          // console.table(res);
           // console.log(query.sql);
-          // howMuch();
-          connection.end();
+          if (res[0].stock_quantity > 0) {
+            console.table(res);
+            // console.log(res[0].stock_quantity);
+            howMuch(idNumber);
+          }
+          if (res[0].stock_quantity < 1) {
+            // console.table(res);
+            console.log("Insufficient quantity!");
+            what();
+          }
+
+          // Still need to figure out how to handle undefined id number
+          //    else {
+          //     // console.table(res);
+          //     console.log("Id number not found. Try again.");
+          //     // what();
+          //   }
+
+          // connection.end();
         }
       );
-      // if (query.sql.id[idNumber]) {
-      //     if
-      //     howMuch();
-      // }
+    });
+}
 
-      // else (
-      //     console.log("That item is no longer available.")
-      // )
+function howMuch(identifier) {
+  inquirer
+    .prompt({
+      name: "amount",
+      type: "number",
+      message: "How much would you like to buy?",
+    })
+    .then(function (answer) {
+      var number = answer.amount;
+
+      // Determine cost
+      var query1 = connection.query(
+        "SELECT * FROM products WHERE ?",
+        { id: identifier },
+        function (err, res) {
+          if (err) throw err;
+          var cost = number * res[0].price;
+          console.log("That will be $" + cost + ".");
+        }
+      );
+
+      connection.end();
+      // Subtract from total number
+      // var query2 = connection.query("SUBTRACT")
     });
 }
